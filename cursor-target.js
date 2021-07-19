d498f8b3ade8f23db9ec0075e423bac28271dc6e
rafern <rafern@protonmail.com>
2021-07-19 15:02:02 +0100

    Add onCursorMove, onCursorDown, onCursorUp, (...) - Use const instead of let when possible - onClick is not the same as onPointerDown + onPointerUp (!!!) - XR controller select also does onPointerDown and onPointerUp

--- cursor-target.js
+++ cursor-target.js
@@ -1,8 +1,11 @@
 /**
- * Click/hover target for [cursor](#cursor).
+ * Click/hover/move/button target for [cursor](#cursor).
  *
- * To trigger code when clicking or hovering, use `.addHoverFunction(f)`,
- * `.addUnHoverFunction(f)` and `.addClickFunction(f)` with any `function f() {}`.
+ * To trigger code when clicking, hovering, unhovering, moving cursor, pressing
+ * cursor button or releasing cursor button, use `.addClickFunction(f)`,
+ * `.addHoverFunction(f)`, `.addUnHoverFunction(f)`,
+ * `.addCursorMoveFunction(f)`, `.addCursorDownFunction(f)` and
+ * `.addCursorUpFunction(f)` respectively with any `function f() {}`.
  *
  * To call members on a different component, you can set up a cursor target like
  * so:
@@ -29,6 +32,15 @@
  *
  * addClickFunction(callback);
  * removeClickFunction(callback);
+ *
+ * addCursorMoveFunction(callback);
+ * removeCursorMoveFunction(callback);
+ *
+ * addCursorDownFunction(callback);
+ * removeCursorDownFunction(callback);
+ *
+ * addCursorUpFunction(callback);
+ * removeCursorUpFunction(callback);
  * ```
  *
  * **Requirements:**
@@ -42,6 +54,9 @@ WL.registerComponent("cursor-target", {
       this.hoverFunctions = [];
       this.unHoverFunctions = [];
       this.clickFunctions = [];
+      this.cursorMoveFunctions = [];
+      this.cursorDownFunctions = [];
+      this.cursorUpFunctions = [];
     },
     onHover: function(object, cursor) {
         for(let i in this.hoverFunctions) {
@@ -58,53 +73,80 @@ WL.registerComponent("cursor-target", {
             this.clickFunctions[i](object, cursor);
         }
     },
-    addHoverFunction: function(f) {
-        if(typeof f !== "function") {
-            throw new TypeError(this.object.name
-                + ".cursor-target: Argument needs to be a function");
+    onCursorMove: function(object, cursor) {
+        for(let i in this.cursorMoveFunctions) {
+            this.cursorMoveFunctions[i](object, cursor);
         }
+    },
+    onCursorDown: function(object, cursor) {
+        for(let i in this.cursorDownFunctions) {
+            this.cursorDownFunctions[i](object, cursor);
+        }
+    },
+    onCursorUp: function(object, cursor) {
+        for(let i in this.cursorUpFunctions) {
+            this.cursorUpFunctions[i](object, cursor);
+        }
+    },
+    addHoverFunction: function(f) {
+        this._validateCallback(f);
         this.hoverFunctions.push(f);
     },
     removeHoverFunction: function(f) {
-        if(typeof f !== "function") {
-            throw new TypeError(this.object.name
-                + ".cursor-target: Argument needs to be a function");
-        }
+        this._validateCallback(f);
         this._removeItemOnce(this.hoverFunctions, f);
     },
     addUnHoverFunction: function(f) {
-        if(typeof f !== "function") {
-            throw new TypeError(this.object.name
-                + ".cursor-target: Argument needs to be a function");
-        }
+        this._validateCallback(f);
         this.unHoverFunctions.push(f);
     },
     removeUnHoverFunction: function(f) {
-        if(typeof f !== "function") {
-            throw new TypeError(this.object.name
-                + ".cursor-target: Argument needs to be a function");
-        }
+        this._validateCallback(f);
         this._removeItemOnce(this.unHoverFunctions, f);
     },
     addClickFunction: function(f) {
-        if(typeof f !== "function") {
-            throw new TypeError(this.object.name
-                + ".cursor-target: Argument needs to be a function");
-        }
+        this._validateCallback(f);
         this.clickFunctions.push(f);
     },
     removeClickFunction: function(f) {
-        if(typeof f !== "function") {
-            throw new TypeError(this.object.name
-                + ".cursor-target: Argument needs to be a function");
-        }
+        this._validateCallback(f);
         this._removeItemOnce(this.clickFunctions, f);
+    },
+    addCursorMoveFunction: function(f) {
+        this._validateCallback(f);
+        this.cursorMoveFunctions.push(f);
+    },
+    removeCursorMoveFunction: function(f) {
+        this._validateCallback(f);
+        this._removeItemOnce(this.cursorMoveFunctions, f);
+    },
+    addCursorDownFunction: function(f) {
+        this._validateCallback(f);
+        this.cursorDownFunctions.push(f);
+    },
+    removeCursorDownFunction: function(f) {
+        this._validateCallback(f);
+        this._removeItemOnce(this.cursorDownFunctions, f);
+    },
+    addCursorUpFunction: function(f) {
+        this._validateCallback(f);
+        this.cursorUpFunctions.push(f);
+    },
+    removeCursorUpFunction: function(f) {
+        this._validateCallback(f);
+        this._removeItemOnce(this.cursorUpFunctions, f);
     },
 
     _removeItemOnce: function(arr, value) {
         var index = arr.indexOf(value);
         if(index > -1) arr.splice(index, 1);
         return arr;
-    }
+    },
+    _validateCallback: function(f) {
+        if(typeof f !== "function") {
+            throw new TypeError(this.object.name
+                + ".cursor-target: Argument needs to be a function");
+        }
+    },
 });
 
