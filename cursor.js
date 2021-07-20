77cd8e8ec92686f9b872797752ebccb4cb074dd2
rafern <rafern@protonmail.com>
2021-07-20 11:30:16 +0100

    Projection should update when entering XR, not when pressing select

--- cursor.js
+++ cursor.js
@@ -248,6 +248,9 @@ WL.registerComponent('cursor', {
         s.addEventListener('select', this.onSelect.bind(this));
         s.addEventListener('selectstart', this.onSelectStart.bind(this));
         s.addEventListener('selectend', this.onSelectEnd.bind(this));
+
+        /* After AR session was entered, the projection matrix changed */
+        this.onViewportResize();
     },
 
     /** 'select' event listener */
@@ -259,8 +262,6 @@ WL.registerComponent('cursor', {
     /** 'selectstart' event listener */
     onSelectStart: function(e) {
         this.arTouchDown = true;
-        /* After AR session was entered, the projection matrix changed */
-        this.onViewportResize();
         if(e.inputSource.handedness == this.handedness) this.isDown = true;
     },
 
