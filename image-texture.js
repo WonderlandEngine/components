3de80f4f6d95fa0e58c5f1a724620676dff95254
Squareys <squareys@googlemail.com>
2022-09-05 11:10:23 +0200

    image-texture: Unify image-texture with video-texture
    Signed-off-by: Squareys <squareys@googlemail.com>

--- image-texture.js
+++ image-texture.js
@@ -5,26 +5,33 @@
  *
  * **Warning:** This component will soon be changed to be consistent with
  *   [video-texture](#video-texture) and change a material rather than mesh.
- *   To keep backwards compatibility, please copy the source of this component
- *   into your project.
+ *   To make sure your code keeps working in future versions of the engine,
+ *   please use `material` rather than `meshIndex`.
  */
 WL.registerComponent('image-texture', {
     /** URL to download the image from */
     url: {type: WL.Type.String, default: ""},
-    /** 0-based mesh component index on this object (e.g. 1 for "second mesh") */
-    meshIndex: {type: WL.Type.Int, default: 0}
+    /** 0-based mesh component index on this object (e.g. 1 for "second mesh").
+     * **Deprecated:** Please use `material` instead. */
+    meshIndex: {type: WL.Type.Int, default: 0},
+    /** Material to apply the video texture to (if `null`, tries to apply to the mesh with `meshIndex`) */
+    material: {type: WL.Type.Material},
 }, {
     init: function() {
-        let obj = this.object;
+        if(!this.material) {
+            console.warn("image-texture: material property not set, please set 'material' instead of deprecated 'meshIndex'.");
+            this.material = this.object.getComponent("mesh", this.meshIndex).material;
+        }
+    },
+    start: function() {
         WL.textures.load(this.url, 'anonymous')
-            .then(function(texture) {
-                const mat = obj.getComponent("mesh", this.meshIndex).material;
-                if(mat.shader == "Flat Opaque Textured") {
-                    mat.flatTexture = texture;
+            .then((texture) => {
+                if(this.material.shader == "Flat Opaque Textured") {
+                    this.material.flatTexture = texture;
                 } else if(mat.shader == "Phong Opaque Textured") {
-                    mat.diffuseTexture = texture;
+                    this.material.diffuseTexture = texture;
                 } else {
-                    console.error("Shader", mat.shader, "not supported by image-texture");
+                    console.error("Shader", this.material.shader, "not supported by image-texture");
                 }
             }).catch(console.err);
     }
