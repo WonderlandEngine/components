2492ddac5438e7c28ef212316d4071676c1fc778
NSTCG <nithinsteven32@gmail.com>
2023-08-02 15:04:05 +0530

    js: Improve input-profile.js
    Fix minor errors and improve w.r.t conducted test cases

--- input-profile.js
+++ input-profile.js
@@ -12,11 +12,14 @@ export class InputProfile extends Component {
             'https://cdn.jsdelivr.net/npm/@webxr-input-profiles/assets@1.0.13/dist/profiles/'
         ),
         customProfileFolder: Property.string(''),
+        preSpawnedController: Property.object(),
     };
 
     init() {
-        this._gamepadObjects = {}; 
+        this._gamepadObjects = {};
         this._modelLoaded = false;
+        this.controllerDeactivateFlag = false;
+        this.ProfileJSON = {};
     }
 
     start() {
@@ -35,6 +38,11 @@ export class InputProfile extends Component {
         }
 
         this.handedness = ['left', 'right'][this.handednessIndex];
+
+        this.components = [];
+        if (this.preSpawnedController != null) {
+            this.getComponents(this.preSpawnedController);
+        }
     }
 
     update() {
@@ -45,41 +53,106 @@ export class InputProfile extends Component {
 
     onInputSourcesChange(event) {
         event.added.forEach((xrInputSource) => {
-            const profile = this.customProfileFolder || xrInputSource.profiles[0];
-            /** load Profile json */
-            /** Note: if you are providing a custom ProfileJSON provide its path in the
-             * customProfileFolder property of the input-profile component.
-             */
-            this.url = this.path + profile + '/profile.json';
+            if (xrInputSource.hand != undefined) {
+                this.handleHandSource();
+                return;
+            } else {
+                this.handleControllerSource(xrInputSource);
+            }
+        });
+    }
+
+    handleControllerSource(xrInputSource) {
+        this.activatePrespawnedControllersOnStart();
+        this.loadProfileJson(xrInputSource);
+    }
+
+    handleHandSource(xrInputSource) {
+        this.deactivatePreSpawnedControllerOnStart();
+    }
+
+    activatePrespawnedControllersOnStart() {
+        this.setComponentsActive(true);
+    }
+
+    deactivatePreSpawnedControllerOnStart() {
+        this.setComponentsActive(false);
+    }
+
+    getComponents(obj) {
+        const comps = obj.getComponents().filter((c) => c.type !== 'vr-mode-active-switch');
+        this.components = this.components.concat(comps);
+
+        let children = obj.children;
+        for (let i = 0; i < children.length; ++i) {
+            this.getComponents(children[i]);
+        }
+    }
+
+    setComponentsActive(active) {
+        const comps = this.components;
+        for (let i = 0; i < comps.length; ++i) {
+            comps[i].active = active;
+        }
+    }
+
+    loadProfileJson(xrInputSource) {
+        /** load Profile json */
+        /** Note: if you are providing a custom ProfileJSON provide its path in the
+         * customProfileFolder property of the input-profile component.
+         */
+        this.profile = this.customProfileFolder || this.path + xrInputSource.profiles[0];
+        this.url = this.profile + '/profile.json';
+        if (Object.keys(this.ProfileJSON).length > 0) {
+            this.loadModel(xrInputSource);
+        } else {
             fetch(this.url)
                 .then((res) => res.json())
                 .then((out) => {
                     this.ProfileJSON = out;
-                    const consoleText = this.modelLoaded
-                        ? 'Profile loaded' + this.url 
-                        : 'Reloaded Profile for gamepad mapping ';
-                    console.log(consoleText);
+                    this._profileJsonLoaded = true;
+                    this.loadModel(xrInputSource);
                 })
-                .catch(console.error);
+                .catch((error) => {
+                    console.log('couldnot read profile.json ' + this.url);
+                });
+        }
+    }
 
-            if (this.handedness === xrInputSource.handedness) {
-                this.gamepad = xrInputSource.gamepad;
-                /** load controllerModel **/
-                const assetPath = this.path + profile + '/' + this.handedness + '.glb';
-                if (!this.modelLoaded) {
-                    this.engine.scene
-                        .append(assetPath)
-                        .then((obj) => {
-                            obj.parent = this.object;
-                            obj.setTranslationLocal([0, 0, 0]);
-                            this.getGamepadObjectsFromProfile(this.ProfileJSON, obj);
-                            this.modelLoaded = true;
-                            console.log('model loaded to the scene');
-                        })
-                        .catch(console.error);
-                }
+    loadModel(xrInputSource) {
+        if (this.handedness === xrInputSource.handedness) {
+            this.gamepad = xrInputSource.gamepad;
+            /** load controllerModel **/
+
+            if (this.modelLoaded) return;
+
+            if (this.preSpawnedController !== null) {
+                this.controllerObject = this.preSpawnedController;
+                this.controllerObject.parent = this.object;
+                this.controllerObject.setTranslationLocal([0, 0, 0]);
+                this.getGamepadObjectsFromProfile(this.ProfileJSON, this.controllerObject);
+                this.modelLoaded = true;
+                console.log('Loaded ' + this.handedness + '  controller');
+            } else {
+                const assetPath = this.profile + '/' + this.handedness + '.glb';
+                this.engine.scene
+                    .append(assetPath)
+                    .then((obj) => {
+                        this.controllerObject = obj;
+                        this.components = [];
+                        this.getComponents(obj);
+                        this.controllerObject.parent = this.object;
+                        this.controllerObject.setTranslationLocal([0, 0, 0]);
+                        this.getGamepadObjectsFromProfile(
+                            this.ProfileJSON,
+                            this.controllerObject
+                        );
+                        this.modelLoaded = true;
+                        console.log('Loaded ' + this.handedness + '  controller');
+                    })
+                    .catch(console.error);
             }
-        });
+        }
     }
 
     getGamepadObjectsFromProfile(profile, obj) {
