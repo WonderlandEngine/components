615d4e91e4fbaef99d911a1e5b569f17f34c7edf
Squareys <squareys@googlemail.com>
2023-08-08 15:20:46 +0700

    mouse-look: Modernize and fix deactivation behavior
    Signed-off-by: Squareys <squareys@googlemail.com>

--- mouse-look.js
+++ mouse-look.js
@@ -1,8 +1,8 @@
-import {Component, Type} from '@wonderlandengine/api';
+import {Component, Property} from '@wonderlandengine/api';
 import {vec3} from 'gl-matrix';
 
 /**
- * Controls the camera through mouse movement.
+ * Controls the camera oriantation via mouse movement.
  *
  * Efficiently implemented to affect object orientation only
  * when the mouse moves.
@@ -10,93 +10,111 @@ import {vec3} from 'gl-matrix';
 export class MouseLookComponent extends Component {
     static TypeName = 'mouse-look';
     static Properties = {
-        /** Mouse look sensitivity */
-        sensitity: {type: Type.Float, default: 0.25},
+        /** Mouse look sensitivity FIXME typo */
+        sensitity: Property.float(0.25),
         /** Require a mouse button to be pressed to control view.
          * Otherwise view will allways follow mouse movement */
-        requireMouseDown: {type: Type.Bool, default: true},
+        requireMouseDown: Property.bool(true),
         /** If "moveOnClick" is enabled, mouse button which should
          * be held down to control view */
-        mouseButtonIndex: {type: Type.Int},
+        mouseButtonIndex: Property.int(),
         /** Enables pointer lock on "mousedown" event on canvas */
-        pointerLockOnClick: {type: Type.Bool, default: false},
+        pointerLockOnClick: Property.bool(false),
     };
 
-    init() {
-        this.currentRotationY = 0;
-        this.currentRotationX = 0;
-        this.origin = new Float32Array(3);
-        this.parentOrigin = new Float32Array(3);
-        this.rotationX = 0;
-        this.rotationY = 0;
-    }
+    currentRotationY = 0;
+    currentRotationX = 0;
+    origin = new Float32Array(3);
+    parentOrigin = new Float32Array(3);
+    rotationX = 0;
+    rotationY = 0;
 
-    start() {
-        document.addEventListener('mousemove', (e) => {
-            if (this.active && (this.mouseDown || !this.requireMouseDown)) {
-                this.rotationY = (-this.sensitity * e.movementX) / 100;
-                this.rotationX = (-this.sensitity * e.movementY) / 100;
-
-                this.currentRotationX += this.rotationX;
-                this.currentRotationY += this.rotationY;
-
-                /* 1.507 = PI/2 = 90° */
-                this.currentRotationX = Math.min(1.507, this.currentRotationX);
-                this.currentRotationX = Math.max(-1.507, this.currentRotationX);
-
-                this.object.getTranslationWorld(this.origin);
-
-                const parent = this.object.parent;
-                if (parent !== null) {
-                    parent.getTranslationWorld(this.parentOrigin);
-                    vec3.sub(this.origin, this.origin, this.parentOrigin);
-                }
-
-                this.object.resetTranslationRotation();
-                this.object.rotateAxisAngleRad([1, 0, 0], this.currentRotationX);
-                this.object.rotateAxisAngleRad([0, 1, 0], this.currentRotationY);
-                this.object.translate(this.origin);
-            }
-        });
+    onActivate() {
+        document.addEventListener('mousemove', this.onMouseMove);
 
         const canvas = this.engine.canvas;
         if (this.pointerLockOnClick) {
-            canvas.addEventListener('mousedown', () => {
-                canvas.requestPointerLock =
-                    canvas.requestPointerLock ||
-                    canvas.mozRequestPointerLock ||
-                    canvas.webkitRequestPointerLock;
-                canvas.requestPointerLock();
-            });
+            canvas.addEventListener('mousedown', this.requestPointerLock);
         }
-
         if (this.requireMouseDown) {
             if (this.mouseButtonIndex == 2) {
-                canvas.addEventListener(
-                    'contextmenu',
-                    (e) => {
-                        e.preventDefault();
-                    },
-                    false
-                );
+                canvas.addEventListener('contextmenu', this.preventDefault, false);
             }
-            canvas.addEventListener('mousedown', (e) => {
-                if (e.button == this.mouseButtonIndex) {
-                    this.mouseDown = true;
-                    document.body.style.cursor = 'grabbing';
-                    if (e.button == 1) {
-                        e.preventDefault();
-                        /* Prevent scrolling */
-                        return false;
-                    }
-                }
-            });
-            canvas.addEventListener('mouseup', (e) => {
-                if (e.button == this.mouseButtonIndex) {
-                    this.mouseDown = false;
-                    document.body.style.cursor = 'initial';
-                }
-            });
+            canvas.addEventListener('mousedown', this.onMouseDown);
+            canvas.addEventListener('mouseup', this.onMouseUp);
         }
     }
+
+    onDeactivate() {
+        document.removeEventListener('mousemove', this.onMouseMove);
+
+        const canvas = this.engine.canvas;
+        if (this.pointerLockOnClick) {
+            canvas.removeEventListener('mousedown', this.requestPointerLock);
+        }
+        if (this.requireMouseDown) {
+            if (this.mouseButtonIndex == 2) {
+                canvas.removeEventListener('contextmenu', this.preventDefault, false);
+            }
+            canvas.removeEventListener('mousedown', this.onMouseDown);
+            canvas.removeEventListener('mouseup', this.onMouseUp);
+        }
+    }
+
+    onMouseMove = (e) => {
+        if (!this.mouseDown && this.requireMouseDown) return;
+        this.rotationY = (-this.sensitity * e.movementX) / 100;
+        this.rotationX = (-this.sensitity * e.movementY) / 100;
+
+        this.currentRotationX += this.rotationX;
+        this.currentRotationY += this.rotationY;
+
+        /* 1.507 = PI/2 = 90° */
+        this.currentRotationX = Math.min(1.507, this.currentRotationX);
+        this.currentRotationX = Math.max(-1.507, this.currentRotationX);
+
+        this.object.getPositionWorld(this.origin);
+
+        const parent = this.object.parent;
+        if (parent !== null) {
+            parent.getPositionWorld(this.parentOrigin);
+            vec3.sub(this.origin, this.origin, this.parentOrigin);
+        }
+
+        this.object.resetPositionRotation();
+        this.object.rotateAxisAngleRadLocal([1, 0, 0], this.currentRotationX);
+        this.object.rotateAxisAngleRadLocal([0, 1, 0], this.currentRotationY);
+        this.object.translateLocal(this.origin);
+    };
+
+    onMouseUp = (e) => {
+        if (e.button != this.mouseButtonIndex) return;
+        this.mouseDown = false;
+        document.body.style.cursor = 'initial';
+    };
+
+    onMouseDown = (e) => {
+        if (e.button != this.mouseButtonIndex) return;
+        this.mouseDown = true;
+        document.body.style.cursor = 'grabbing';
+        if (e.button == 1) {
+            e.preventDefault();
+            /* Prevent scrolling */
+            return false;
+        }
+    };
+
+    /* Requests a pointer lock on the canvas DOM element */
+    requestPointerLock = () => {
+        canvas.requestPointerLock =
+            canvas.requestPointerLock ||
+            canvas.mozRequestPointerLock ||
+            canvas.webkitRequestPointerLock;
+        canvas.requestPointerLock();
+    };
+
+    /* Prevents default browser behavior */
+    preventDefault = () => {
+        e.preventDefault();
+    };
 }
