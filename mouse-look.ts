7527e729341a11533a55a0304eb77bbe92318fa8
CD <113025490+wle-cd@users.noreply.github.com>
2025-05-13 15:44:17 +0900

    Run prettier

--- mouse-look.ts
+++ mouse-look.ts
@@ -2,7 +2,9 @@ import {Component} from '@wonderlandengine/api';
 import {property} from '@wonderlandengine/api/decorators.js';
 import {quat} from 'gl-matrix';
 
-const preventDefault = (e: Event) => { e.preventDefault(); };
+const preventDefault = (e: Event) => {
+    e.preventDefault();
+};
 
 const TEMP_ROT = new Float32Array(4);
 const ROT_MUL = 180 / Math.PI / 100;
@@ -79,7 +81,7 @@ export class MouseLookComponent extends Component {
             (canvas as any).mozRequestPointerLock ||
             (canvas as any).webkitRequestPointerLock;
         canvas.requestPointerLock();
-    }
+    };
 
     onMouseDown = (e: MouseEvent) => {
         if (e.button === this.mouseButtonIndex) {
@@ -91,24 +93,24 @@ export class MouseLookComponent extends Component {
                 return false;
             }
         }
-    }
+    };
 
     onMouseUp = (e: MouseEvent) => {
         if (e.button === this.mouseButtonIndex) {
             this.mouseDown = false;
             document.body.style.cursor = 'initial';
         }
-    }
+    };
 
     onMouseMove = (e: MouseEvent) => {
         if (this.active && (this.mouseDown || !this.requireMouseDown)) {
-            this.currentRotationX += (-this.sensitity * e.movementY) * ROT_MUL;
-            this.currentRotationY += (-this.sensitity * e.movementX) * ROT_MUL;
+            this.currentRotationX += -this.sensitity * e.movementY * ROT_MUL;
+            this.currentRotationY += -this.sensitity * e.movementX * ROT_MUL;
             // 89 deg instead of 90 so that there are no camera glitches
             // when looking straight down/up
             this.currentRotationX = Math.max(-89, Math.min(89, this.currentRotationX));
             quat.fromEuler(TEMP_ROT, this.currentRotationX, this.currentRotationY, 0);
             this.object.setRotationLocal(TEMP_ROT);
         }
-    }
+    };
 }
